aca262914cc74387d38d3ab1b1826719f4f19ac2
Ify Ogwu <ifyogwu@gmail.com>
2025-12-01 14:51:49 +0800

    1/12/25 Added unittest file and smaller changes thorughout project

--- tests/carpark_test.py
+++ tests/carpark_test.py
@@ -1,22 +1,40 @@
 import unittest
+import tempfile
 import sys,os
+import logging
 from pathlib import Path
 cwd = Path(os.path.dirname(__file__))
 parent = str(cwd.parent)
 
 sys.path.append(parent + "/smartpark")
 
-#Change the line below to import your manager class
-from carpark_manager import CarparkManager
 
-class TestConfigParsing(unittest.TestCase):
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, PROJECT_ROOT)
+
+#Change the line below to import your manager class
+from smartpark.carpark_manager import CarparkManager, Car
+
+
+class TestProject(unittest.TestCase):
 
     def test_fresh_carpark(self):
+        """Test 1: config.json if being read correctly"""
         # arrange
+        """Set config["CarParks]["free-spaces] = 1000"""
         # act
-        carpark = MockCarparkManager()
+        carpark = CarparkManager()
         # assert
         self.assertEqual(1000,carpark.available_spaces)
+
+    def test_temp(self):
+        """Test 2: config.json if being read correctly"""
+        # arrange
+        """Set config["CarParks]["temperature] = 12.0"""
+        # act
+        carpark = CarparkManager()
+        # assert
+        self.assertEqual(12.0,carpark.temperature)
 
 if __name__=="__main__":
 #    print("cwd: " + parent + "/smartpark")
